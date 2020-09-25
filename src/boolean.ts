4336d9698bfd630155b2a08a4ad581cc8bb6d03f
hana-sama <hmama8989@gmail.com>
2020-09-25 15:54:18 +0900

    upgrading ts-node

---
+++ src/boolean.ts
@@ -0,0 +1,8 @@
+export {};
+let isFinished: boolean = true;
+isFinished = false;
+let number: number = 37;
+let name: string = "Hana";
+console.log({ isFinished });
+console.log({ number });
+console.log({ name });
